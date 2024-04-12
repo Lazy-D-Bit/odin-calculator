efdf8f70e1fdc449091982cb8d0b87406cd10c08
Lazy-D-Bit <161049991+Lazy-D-Bit@users.noreply.github.com>
2024-04-12 17:17:35 +0530

    Shows the numbers Typed on Screen

--- script.js
+++ script.js
@@ -20,3 +20,20 @@ function operate(operator,firstsNumber,secondNumber) {
         return add(firstsNumber,secondNumber);
     }
 }
+
+const operand = document.getElementById("operand");
+document.getElementById("btn-c").addEventListener("click", function() {
+    operand.innerText = "0";
+    
+});
+const number = document.querySelectorAll(".number");
+number.forEach(number => {
+    number.addEventListener("click", function(event) {
+        const btn = event.target.id; // Get the ID of the clicked button
+        if (operand.innerText == "0") {
+            operand.innerText = "";
+        }
+        const i = btn[4]
+        operand.innerText += i;
+    });
+});
